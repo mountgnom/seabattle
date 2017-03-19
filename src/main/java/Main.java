5c52550522cd598f8e7a0c94866e4af692cd7369
tdzhahangirov <tdzhahangirov@programma-t.ru>
2017-03-19 22:05:38 +0300

    replace interfaces

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Created by TDzhahangirov on 19.03.2017.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
---
+++ src/main/java/game/Board.java
@@ -0,0 +1,41 @@
+package game;
+
+import game.Ship;
+
+import javax.management.MXBean;
+import java.util.List;
+
+/**
+ * Игровая доска, на ней располагаются корабли.
+ *
+ * Created by TDzhahangirov on 15.03.2017.
+ */
+@MXBean
+public interface Board {
+
+    /**
+     * создание нового игрока. В реализации интерфейса должно быть ограничение на создание не более чем 2х игроков
+     * @return
+     */
+    public Gamer createGamer();
+
+
+    @MXBean
+    public interface Gamer{
+
+
+
+        /**
+         *
+         * @return список кораблей игрока
+         */
+        List<Ship> getShips();
+
+        /**
+         * устанаваливает список караблей игрока
+         * @param ships
+         */
+        void setShips(List<Ship> ships);
+    };
+
+}
